c112eb935fa082b2232cccc22c86843d8dbb93a3
haeunNoh06 <okhagrace06@naver.com>
2024-08-20 09:44:20 +0900

    상태표시줄 작성

--- WinformAdv/Form1.cs
+++ WinformAdv/Form1.cs
@@ -16,5 +16,10 @@ namespace WinformAdv
         {
             InitializeComponent();
         }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
