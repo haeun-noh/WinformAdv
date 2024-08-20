5129ed37dbb32f393f3bb15b0aef119f408671cc
haeunNoh06 <okhagrace06@naver.com>
2024-08-20 10:15:28 +0900

    보기 > 확대-축소 서브메뉴 구현

--- WinformAdv/Form1.Designer.cs
+++ WinformAdv/Form1.Designer.cs
@@ -317,7 +317,7 @@
             this.축소ToolStripMenuItem,
             this.확대하기축소하기기본값복원ToolStripMenuItem});
             this.확대하기축소하기ToolStripMenuItem.Name = "확대하기축소하기ToolStripMenuItem";
-            this.확대하기축소하기ToolStripMenuItem.Size = new System.Drawing.Size(218, 26);
+            this.확대하기축소하기ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.확대하기축소하기ToolStripMenuItem.Text = "확대하기/축소하기";
             // 
             // 확대ToolStripMenuItem
@@ -325,18 +325,21 @@
             this.확대ToolStripMenuItem.Name = "확대ToolStripMenuItem";
             this.확대ToolStripMenuItem.Size = new System.Drawing.Size(303, 26);
             this.확대ToolStripMenuItem.Text = "확대";
+            this.확대ToolStripMenuItem.Click += new System.EventHandler(this.확대ToolStripMenuItem_Click);
             // 
             // 축소ToolStripMenuItem
             // 
             this.축소ToolStripMenuItem.Name = "축소ToolStripMenuItem";
             this.축소ToolStripMenuItem.Size = new System.Drawing.Size(303, 26);
             this.축소ToolStripMenuItem.Text = "축소";
+            this.축소ToolStripMenuItem.Click += new System.EventHandler(this.축소ToolStripMenuItem_Click);
             // 
             // 확대하기축소하기기본값복원ToolStripMenuItem
             // 
             this.확대하기축소하기기본값복원ToolStripMenuItem.Name = "확대하기축소하기기본값복원ToolStripMenuItem";
             this.확대하기축소하기기본값복원ToolStripMenuItem.Size = new System.Drawing.Size(303, 26);
             this.확대하기축소하기기본값복원ToolStripMenuItem.Text = "확대하기/축소하기 기본값 복원";
+            this.확대하기축소하기기본값복원ToolStripMenuItem.Click += new System.EventHandler(this.확대하기축소하기기본값복원ToolStripMenuItem_Click);
             // 
             // 상태표시줄ToolStripMenuItem
             // 
@@ -402,7 +405,7 @@
             this.toolStripProgressBar1.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.toolStripProgressBar1.Name = "toolStripProgressBar1";
             this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 18);
-            this.toolStripProgressBar1.Value = 100;
+            this.toolStripProgressBar1.Value = 50;
             // 
             // textBox1
             // 
